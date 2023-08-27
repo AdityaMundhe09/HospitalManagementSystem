aa780afc493928aa1010d5452cc607944167726b
AdityaMundhe09 <adityamundhe6993@gmail.com>
2023-08-27 22:33:26 +0530

    update with testing

--- Backend/HospitalManagementSystemBackend/src/main/java/com/app/Repository/DoctorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.entities.Doctor;
-
-public interface DoctorRepository extends JpaRepository<Doctor, Integer>{
-
-}
